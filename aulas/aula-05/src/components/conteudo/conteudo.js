3daffe3513727fe02cc13e0e358e981a45d16793
LUCAS SANTOS CAMPOS <lucas.scampos4@senacsp.edu.br>
2023-03-15 10:49:54 -0300

    Add os objetos, mas tem que se decidir qual tipo vai ser utilizado.

--- aulas/aula-05/src/components/conteudo/conteudo.js
+++ aulas/aula-05/src/components/conteudo/conteudo.js
@@ -1,4 +1,27 @@
+import React from "react";
+
 function Conteudo() {
+
+
+    const [palavra, setPalavra] = React.useState();
+
+
+    const palavras = {
+        "T.I": "CLAUDIA",
+        "design": ["NELSON"]
+    }
+
+
+    function verifica() {
+        console.log(palavras.design[0])
+        if (palavra == palavras["T.I"][0]) {
+            console.log("entrou")
+        }
+        
+    }
+
+
+
     return (
         <>
             <div className="card m-5">
@@ -10,13 +33,16 @@ function Conteudo() {
                             <label className="col-auto">Geral (por agora :v)</label>
                         </div>
                         <div className="col" >
-                            <input type="text" className="form-control" placeholder="Digite aqui..." />
+                            <input type="text" className="form-control" placeholder="Digite aqui..." onChange={(e) => {setPalavra(e.target.value)}}/>
+            
+                            <p>{palavra}</p>
+
                         </div>
                     </form>
                 </div>
                 <div className="modal-footer flex-column border-top-0">
                     {/* <button type="button" className="btn btn-lg btn-primary w-100 mx-0 mb-2">Save changes</button> */}
-                    <button type="button" className="btn btn-success w-75 m-3 h-auto">Enviar</button>
+                    <button type="button" className="btn btn-success w-75 m-3 h-auto" onClick={verifica}>Enviar</button>
                 </div>
             </div>
         </>
